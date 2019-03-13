b717bc77bd51fbd8f03a6c51932d4051585e708c
Nicky Forster <nforster@ap.org>
2019-03-13 12:50:16 -0400

    added process for getting session token and deleting session token with xmltodict parsing

--- search_satellites.py
+++ search_satellites.py
@@ -1,5 +1,6 @@
 import config
 import requests
+import xmltodict
 
 class Session:
     """main session class for interacting with the nasa cmr"""
@@ -10,8 +11,18 @@ class Session:
         token_url = "https://cmr.earthdata.nasa.gov/legacy-services/rest/tokens"
         payload = f"<token><username>{username}</username><password>{password}</password><client_id>search_satellites</client_id><user_ip_address>192.168.1.1</user_ip_address> </token>"
         headers = { 'Content-Type': 'application/xml' }
-        self.token = requests.post(token_url,data=payload,headers=headers).text
+        resp = requests.post(token_url,data=payload,headers=headers).text
+        resp_dict = xmltodict.parse(resp)
+        self.token = resp_dict['token']['id']
+
+    
+    def delete_token(self):
+        token_url = f"https://cmr.earthdata.nasa.gov/legacy-services/rest/tokens/{self.token}"
+        headers = { 'Content-Type': 'application/xml' }
+        resp = requests.delete(token_url, headers=headers)
+        print(resp.status_code)
 
 cmr_session = Session()
 cmr_session.get_token(config.earthdata_user, config.earthdata_pass)
 print(cmr_session.token)
+cmr_session.delete_token()
